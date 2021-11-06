dea9649a5f921c80ac0de11f4067ecb3bc4ddd0c
Raheel-Saleem <75340373+Raheel-Saleem@users.noreply.github.com>
2021-11-07 00:15:33 +0500

    comit construction

--- src/views/pages/Construction/Account/Account.js
+++ src/views/pages/Construction/Account/Account.js
@@ -3,15 +3,47 @@ import './Account.css';
 import { useDispatch } from 'react-redux';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
-
+import { startLoading, stopLoading } from '../../../../store/actions';
+import swal from 'sweetalert';
+import server from '../../../../server/server';
 const initialValues = {
-    accountNumber: '',
+    accountNo: '',
     name: '',
-    email: '',
     amount: 0
+};
+const validationSchema = Yup.object({
+    accountNo: Yup.string().required('Required!'),
+    name: Yup.string().required('Required!'),
+    amount: Yup.string().required('Required!')
+});
+
+const submitFormValues = async (dispatch, values) => {
+    try {
+        dispatch(startLoading());
+        await server.post('https://property-manag.herokuapp.com/constructionAmount', values);
+        dispatch(stopLoading());
+        swal('Success!', 'Construction Account Open !', 'success');
+    } catch (error) {
+        dispatch(stopLoading());
+        swal('Error!', 'Something Went Wrong Please TryAgain!', 'error');
+
+        console.log(error);
+    }
 };
 
 const Account = () => {
+    const dispatch = useDispatch();
+
+    const formik = useFormik({
+        initialValues,
+        validationSchema,
+        onSubmit: (values, onSubmitProps) => {
+            console.log('from on submit fun', values);
+            submitFormValues(dispatch, values);
+            onSubmitProps.setSubmitting(false);
+            onSubmitProps.resetForm();
+        }
+    });
     return (
         <>
             <div className="d-lg-flex half">
@@ -24,14 +56,23 @@ const Account = () => {
                     </div>
                     <div className="row align-items-center justify-content-center">
                         <div className="inner_box col-md-6 py-4">
-                            <form action="#" method="post">
+                            <form onSubmit={formik.handleSubmit}>
                                 <div className="row">
                                     <div className="col-md-12">
                                         <div className="form-group first">
                                             <label className="input_heading" htmlFor="account">
                                                 <b>Account #</b>
                                             </label>
-                                            <input type="text" className="form-control" placeholder id="account" />
+
+                                            <input
+                                                type="text"
+                                                name="accountNo"
+                                                className="form-control"
+                                                placeholder
+                                                id="account"
+                                                {...formik.getFieldProps('accountNo')}
+                                                required
+                                            />
                                         </div>
                                     </div>
                                 </div>
@@ -41,7 +82,16 @@ const Account = () => {
                                             <label className="input_heading" htmlFor="name">
                                                 <b>Name</b>
                                             </label>
-                                            <input type="text" className="form-control" placeholder id="name" />
+
+                                            <input
+                                                type="text"
+                                                name="name"
+                                                className="form-control"
+                                                placeholder
+                                                id="name"
+                                                {...formik.getFieldProps('name')}
+                                                required
+                                            />
                                         </div>
                                     </div>
                                 </div>
@@ -51,7 +101,13 @@ const Account = () => {
                                             <label className="input_heading" htmlFor="amount">
                                                 <b>Amount</b>
                                             </label>
-                                            <input type="text" className="form-control" placeholder id="email" />
+                                            <input
+                                                required
+                                                type="number"
+                                                name="amount"
+                                                className="form-control"
+                                                {...formik.getFieldProps('amount')}
+                                            />
                                         </div>
                                     </div>
                                 </div>
--- src/views/pages/Construction/AddPlot/AddPlot.js
+++ src/views/pages/Construction/AddPlot/AddPlot.js
@@ -1,6 +1,56 @@
 import React from 'react';
 import './AddPlot.css';
+import { useDispatch } from 'react-redux';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { startLoading, stopLoading } from '../../../../store/actions';
+import swal from 'sweetalert';
+import server from '../../../../server/server';
+
+const initialValues = {
+    societyName: '',
+    plotNo: '',
+    plotOwnerName: '',
+    phoneNo: '',
+    streetLocation: '',
+    categories: '',
+    totalStories: '',
+    plotSqFeet: 0,
+    totalPlotSize: '',
+    ratePerSqFeet: 0,
+    pay: 0,
+    structure: '',
+    material: false,
+    sector: ''
+};
+
+const submitFormValues = async (dispatch, values) => {
+    try {
+        dispatch(startLoading());
+        await server.post('https://property-manag.herokuapp.com/addPlot', values);
+        dispatch(stopLoading());
+        swal('Success!', 'Construction Plot Added Successfully  !', 'success');
+    } catch (error) {
+        dispatch(stopLoading());
+        swal('Error!', 'Something Went Wrong Please TryAgain!', 'error');
+
+        console.log(error);
+    }
+};
 const AddPlot = () => {
+    const dispatch = useDispatch();
+
+    const formik = useFormik({
+        initialValues,
+        // validationSchema,
+        onSubmit: (values, onSubmitProps) => {
+            console.log('from on submit fun', values);
+            submitFormValues(dispatch, values);
+            onSubmitProps.setSubmitting(false);
+            onSubmitProps.resetForm();
+        }
+    });
+
     return (
         <div className="d-lg-flex half">
             <div className="container">
@@ -12,14 +62,22 @@ const AddPlot = () => {
                 </div>
                 <div className="row align-items-center justify-content-center">
                     <div className="inner_box col-md-6 py-4">
-                        <form action="#" method="post">
+                        <form onSubmit={formik.handleSubmit}>
                             <div className="row">
                                 <div className="col-md-6">
                                     <div className="form-group first">
                                         <label className="input_heading" htmlFor="account">
                                             <b>Society Name #</b>
                                         </label>
-                                        <input type="text" className="form-control" placeholder id="account" />
+                                        <input
+                                            required
+                                            {...formik.getFieldProps('societyName')}
+                                            type="text"
+                                            className="form-control"
+                                            placeholder
+                                            id="account"
+                                            name="societyName"
+                                        />
                                     </div>
                                 </div>
                                 <div className="col-md-6">
@@ -27,7 +85,15 @@ const AddPlot = () => {
                                         <label className="input_heading" htmlFor="name">
                                             <b>PLot#</b>
                                         </label>
-                                        <input type="text" className="form-control" placeholder id="name" />
+                                        <input
+                                            required
+                                            {...formik.getFieldProps('plotNo')}
+                                            type="text"
+                                            className="form-control"
+                                            placeholder
+                                            id="name"
+                                            name="plotNo"
+                                        />
                                     </div>
                                 </div>
                             </div>
@@ -37,7 +103,15 @@ const AddPlot = () => {
                                         <label className="input_heading" htmlFor="name">
                                             <b>Street Location</b>
                                         </label>
-                                        <input type="text" className="form-control" placeholder id="name" />
+                                        <input
+                                            required
+                                            {...formik.getFieldProps('streetLocation')}
+                                            type="text"
+                                            className="form-control"
+                                            placeholder
+                                            id="name"
+                                            name="streetLocation"
+                                        />
                                     </div>
                                 </div>
                             </div>
@@ -47,11 +121,12 @@ const AddPlot = () => {
                                         <label className="input_heading" htmlFor="amount">
                                             <b>Category</b>
                                         </label>
-                                        <select className="custom-select">
+
+                                        <select className="custom-select" name="categories" {...formik.getFieldProps('categories')}>
                                             <option selected>Category</option>
-                                            <option value={1}>Commercial</option>
-                                            <option value={2}>General</option>
-                                            <option value={3}>Corner</option>
+                                            <option value="commercial">Commercial</option>
+                                            <option value="general">General</option>
+                                            <option value="corner">Corner</option>
                                         </select>
                                     </div>
                                 </div>
@@ -60,11 +135,11 @@ const AddPlot = () => {
                                         <label className="input_heading" htmlFor="amount">
                                             <b>Total Stories</b>
                                         </label>
-                                        <select className="custom-select">
+                                        <select className="custom-select" name="totalStories" {...formik.getFieldProps('totalStories')}>
                                             <option selected>Total Stories</option>
-                                            <option value={1}>Single</option>
-                                            <option value={2}>Double</option>
-                                            <option value={3}>Triple</option>
+                                            <option value="single">Single</option>
+                                            <option value="double">Double</option>
+                                            <option value="triple">Triple</option>
                                         </select>
                                     </div>
                                 </div>
@@ -73,10 +148,10 @@ const AddPlot = () => {
                                         <label className="input_heading" htmlFor="amount">
                                             <b>Structure</b>
                                         </label>
-                                        <select className="custom-select">
+                                        <select className="custom-select" name="structure" {...formik.getFieldProps('structure')}>
                                             <option selected>Structure</option>
-                                            <option value={1}>Grey Structure</option>
-                                            <option value={2}>Finished Structure</option>
+                                            <option value="grey">Grey Structure</option>
+                                            <option value="finshed">Finished Structure</option>
                                         </select>
                                     </div>
                                 </div>
@@ -87,7 +162,15 @@ const AddPlot = () => {
                                         <label className="input_heading" htmlFor="account">
                                             <b>Measurement (sqfeet)</b>
                                         </label>
-                                        <input type="text" className="form-control" placeholder id="account" />
+                                        <input
+                                            required
+                                            {...formik.getFieldProps('plotSqFeet')}
+                                            type="number"
+                                            className="form-control"
+                                            placeholder
+                                            id="account"
+                                            name="plotSqFeet"
+                                        />
                                     </div>
                                 </div>
                                 <div className="col-md-4">
@@ -95,7 +178,15 @@ const AddPlot = () => {
                                         <label className="input_heading" htmlFor="account">
                                             <b>Plot Size</b>
                                         </label>
-                                        <input type="text" className="form-control" placeholder id="account" />
+                                        <input
+                                            required
+                                            {...formik.getFieldProps('totalPlotSize')}
+                                            type="text"
+                                            className="form-control"
+                                            placeholder
+                                            id="account"
+                                            name="totalPlotSize"
+                                        />
                                     </div>
                                 </div>
                                 <div className="col-md-4">
@@ -103,7 +194,15 @@ const AddPlot = () => {
                                         <label className="input_heading" htmlFor="name">
                                             <b>Rate per sqfeet</b>
                                         </label>
-                                        <input type="text" className="form-control" placeholder id="name" />
+                                        <input
+                                            required
+                                            {...formik.getFieldProps('ratePerSqFeet')}
+                                            type="text"
+                                            className="form-control"
+                                            placeholder
+                                            id="name"
+                                            name="ratePerSqFeet"
+                                        />
                                     </div>
                                 </div>
                             </div>
@@ -113,10 +212,10 @@ const AddPlot = () => {
                                         <label className="input_heading" htmlFor="amount">
                                             <b>Material</b>
                                         </label>
-                                        <select className="custom-select">
+                                        <select className="custom-select" name="material" {...formik.getFieldProps('material')}>
                                             <option selected>Material Type</option>
-                                            <option value={1}>With Material</option>
-                                            <option value={2}>Without Material</option>
+                                            <option value={true}>With Material</option>
+                                            <option value={false}>Without Material</option>
                                         </select>
                                     </div>
                                 </div>
@@ -125,7 +224,15 @@ const AddPlot = () => {
                                         <label className="input_heading" htmlFor="account">
                                             <b>Pay</b>
                                         </label>
-                                        <input type="text" className="form-control" placeholder id="account" />
+                                        <input
+                                            required
+                                            type="number"
+                                            className="form-control"
+                                            placeholder
+                                            id="account"
+                                            name="pay"
+                                            {...formik.getFieldProps('pay')}
+                                        />
                                     </div>
                                 </div>
                                 <div className="col-md-4">
@@ -133,7 +240,15 @@ const AddPlot = () => {
                                         <label className="input_heading" htmlFor="name">
                                             <b>Sector No#</b>
                                         </label>
-                                        <input type="text" className="form-control" placeholder id="name" />
+                                        <input
+                                            required
+                                            type="text"
+                                            className="form-control"
+                                            placeholder
+                                            id="name"
+                                            name="sector"
+                                            {...formik.getFieldProps('sector')}
+                                        />
                                     </div>
                                 </div>
                             </div>
@@ -143,7 +258,15 @@ const AddPlot = () => {
                                         <label className="input_heading" htmlFor="account">
                                             <b>Plot Owner Name</b>
                                         </label>
-                                        <input type="text" className="form-control" placeholder id="account" />
+                                        <input
+                                            required
+                                            {...formik.getFieldProps('plotOwnerName')}
+                                            type="text"
+                                            className="form-control"
+                                            placeholder
+                                            id="account"
+                                            name="plotOwnerName"
+                                        />
                                     </div>
                                 </div>
                                 <div className="col-md-6">
@@ -151,7 +274,15 @@ const AddPlot = () => {
                                         <label className="input_heading" htmlFor="name">
                                             <b>Phone# </b>
                                         </label>
-                                        <input type="text" className="form-control" placeholder id="name" />
+                                        <input
+                                            required
+                                            {...formik.getFieldProps('phoneNo')}
+                                            type="text"
+                                            className="form-control"
+                                            placeholder
+                                            id="name"
+                                            name="phoneNo"
+                                        />
                                     </div>
                                 </div>
                             </div>
