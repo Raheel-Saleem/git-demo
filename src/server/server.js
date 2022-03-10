0ce0ea3c3630964a30c13126e73b312eb0e5966d
Raheel Saleem <raheelsaleem470@gmail.com>
2022-03-11 01:24:24 +0500

    new  heroku sever

--- src/server/server.js
+++ src/server/server.js
@@ -3,7 +3,7 @@ import { getToken } from '../utils/token';
 // const { REACT_APP_SERVER_BASE_URL } = process.env;
 // export const baseUrl = "https://alumini-portal.herokuapp.com/";
 // export const baseUrl = 'http://localhost:5000';
-export const baseUrl = 'https://property-manag.herokuapp.com';
+export const baseUrl = 'https://real-estate-managment.herokuapp.com/';
 
 const server = axios.create({
     baseURL: baseUrl
